03ac02cb268229f8618a55de923e79229969a5ee
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-10-16 23:52:35 +0700

    add color bottom sheet

--- app/src/main/java/com/umn/kopicyber/klooyur/pages/ExplorePage.kt
+++ app/src/main/java/com/umn/kopicyber/klooyur/pages/ExplorePage.kt
@@ -2,6 +2,7 @@ package com.umn.kopicyber.klooyur.pages
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomSheetScaffold
@@ -32,7 +33,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun ExplorePage() {
 
@@ -63,14 +64,13 @@ fun ExplorePage() {
                         modifier = Modifier.align(Alignment.Center)
                     )
                 }
-            }
+            },
+        sheetContainerColor = androidx.compose.ui.graphics.Color(0xFF2B2930),
 
         ) { }
-
-
-
     }
 
-
-
 }
+
+
+
