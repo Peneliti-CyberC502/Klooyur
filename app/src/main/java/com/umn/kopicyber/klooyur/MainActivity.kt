11eab48669009408979df467cebe98933330043f
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-10-20 01:16:23 +0700

    feat: make Login Page OwO

--- app/src/main/java/com/umn/kopicyber/klooyur/MainActivity.kt
+++ app/src/main/java/com/umn/kopicyber/klooyur/MainActivity.kt
@@ -3,12 +3,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import android.os.Bundle
 import com.umn.kopicyber.klooyur.navigations.AppNavigation
+import com.umn.kopicyber.klooyur.pages.LoginPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            AppNavigation()
+//            AppNavigation()
+            LoginPage()
         }
     }
 }
