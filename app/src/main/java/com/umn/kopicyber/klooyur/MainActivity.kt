e3b1a3f76cf18519b346c53c77a2c9aff8076026
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-10-20 16:24:17 +0700

    feat: Form Page kurang date sama time picker

--- app/src/main/java/com/umn/kopicyber/klooyur/MainActivity.kt
+++ app/src/main/java/com/umn/kopicyber/klooyur/MainActivity.kt
@@ -3,12 +3,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import android.os.Bundle
 import com.umn.kopicyber.klooyur.navigations.AppNavigation
+import com.umn.kopicyber.klooyur.pages.FormPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            AppNavigation()
+//            AppNavigation()
+            FormPage()
         }
     }
 }
