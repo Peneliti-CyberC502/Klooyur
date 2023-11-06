f279c8ec5dee3c97f3b86dbdb245a78d94407985
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-11-06 22:03:25 +0700

    Benerin Import HomePage

--- app/src/main/java/com/umn/kopicyber/klooyur/pages/HomePage.kt
+++ app/src/main/java/com/umn/kopicyber/klooyur/pages/HomePage.kt
@@ -14,6 +14,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.umn.kopicyber.klooyur.R
 
 @Composable
 fun HomePage() {
@@ -39,7 +52,8 @@ fun HomePage() {
                     verticalArrangement = Arrangement.Bottom,
                 ) {
                     Box(
-                        modifier = Modifier.background(Color(0xFFB3261E),
+                        modifier = Modifier.background(
+                            Color(0xFFB3261E),
                             shape = RoundedCornerShape(8.dp))
                     ) {
                         Text("NEXT DESTINATION",
