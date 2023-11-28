68790e2d647fd465e0889d5a368ffd137cd793c0
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-11-28 15:47:54 +0700

    fix import

--- app/src/main/java/com/umn/kopicyber/klooyur/pages/ProfilePage.kt
+++ app/src/main/java/com/umn/kopicyber/klooyur/pages/ProfilePage.kt
@@ -21,6 +21,18 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.umn.kopicyber.klooyur.R
 
 @Composable
 fun ProfilePage() {
